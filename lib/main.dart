7ab1c9c5db67fe3d9683ba91230f2cc87702e02b
jhshamim5-dev <jhshamim5@gmail.com>
2026-04-29 21:20:33 +0600

    Refactor app structure and update navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
 import 'providers/library_provider.dart';
-import 'widgets/top_bar.dart';
-import 'widgets/floating_nav.dart';
+import 'widgets/bottom_nav_bar.dart';
 import 'screens/home_screen.dart';
 import 'screens/library_screen.dart';
 import 'screens/search_screen.dart';
-import 'screens/details_screen.dart';
 
 void main() {
   runApp(
@@ -15,120 +12,89 @@ void main() {
       providers: [
         ChangeNotifierProvider(create: (_) => LibraryProvider()),
       ],
-      child: const MainApp(),
+      child: const AnimeMangaStreamingApp(),
     ),
   );
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({Key? key}) : super(key: key);
+class AnimeMangaStreamingApp extends StatelessWidget {
+  const AnimeMangaStreamingApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'AnimeHub Clone',
+      title: 'Anime & Manga Hub',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         brightness: Brightness.dark,
         scaffoldBackgroundColor: const Color(0xFF0A0A0A),
-        fontFamily: 'Inter', // Or any sans-serif built in
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        fontFamily: 'Inter',
       ),
-      home: const AppRoot(),
+      home: const MainLayout(),
     );
   }
 }
 
-class AppRoot extends StatefulWidget {
-  const AppRoot({Key? key}) : super(key: key);
+class MainLayout extends StatefulWidget {
+  const MainLayout({Key? key}) : super(key: key);
 
   @override
-  State<AppRoot> createState() => _AppRootState();
+  State<MainLayout> createState() => _MainLayoutState();
 }
 
-class _AppRootState extends State<AppRoot> {
-  TabType _currentTab = TabType.ANIME;
+class _MainLayoutState extends State<MainLayout> {
+  int _currentIndex = 0;
 
-  void _handleSelectMedia(BuildContext context, int id) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (_) => DetailsScreen(
-          mediaId: id,
-          onBack: () => Navigator.pop(context),
-          onSelectMedia: (newId) {
-            Navigator.pop(context); // Pop current details
-            _handleSelectMedia(context, newId); // Push new details
-          },
-        ),
-      ),
-    );
-  }
+  final List<Widget> _screens = [
+    const HomeScreen(type: 'ANIME'),
+    const LibraryScreen(),
+    const HomeScreen(type: 'MANGA'),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFF0A0A0A),
-      body: Stack(
-        children: [
-          // Main Body
-          Positioned.fill(
-            child: _buildBody(),
-          ),
-
-          // Top Nav Overlays the body just like absolute top-0
-          Positioned(
-            top: 0,
-            left: 0,
-            right: 0,
-            child: TopBar(
-              title: _currentTab == TabType.LIBRARY
+      extendBody: true,
+      appBar: AppBar(
+        title: Text(
+          _currentIndex == 0
+              ? 'AnimeHub'
+              : _currentIndex == 1
                   ? 'My Library'
-                  : _currentTab == TabType.ANIME
-                      ? 'AnimeHub'
-                      : 'MangaHub',
-              onSearchClick: () {
-                Navigator.push(
-                  context,
-                  PageRouteBuilder(
-                      pageBuilder: (_, __, ___) => SearchScreen(
-                            onBack: () => Navigator.pop(context),
-                            onSelectMedia: (id) =>
-                                _handleSelectMedia(context, id),
-                            initialType: _currentTab == TabType.MANGA
-                                ? 'MANGA'
-                                : 'ANIME',
-                          ),
-                      transitionsBuilder: (_, animation, __, child) {
-                        return FadeTransition(opacity: animation, child: child);
-                      }),
-                );
-              },
-            ),
-          ),
-
-          // Bottom Nav Overlay
-          FloatingNav(
-            currentTab: _currentTab,
-            onTabChange: (tab) => setState(() => _currentTab = tab),
+                  : 'MangaHub',
+          style: const TextStyle(fontWeight: FontWeight.bold),
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => SearchScreen(
+                      initialType: _currentIndex == 2 ? 'MANGA' : 'ANIME'),
+                ),
+              );
+            },
           )
         ],
       ),
+      body: IndexedStack(
+        index: _currentIndex,
+        children: _screens,
+      ),
+      bottomNavigationBar: FloatingNavBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
     );
-  }
-
-  Widget _buildBody() {
-    switch (_currentTab) {
-      case TabType.ANIME:
-        return HomeScreen(
-            type: 'ANIME',
-            onSelectMedia: (id) => _handleSelectMedia(context, id));
-      case TabType.MANGA:
-        return HomeScreen(
-            type: 'MANGA',
-            onSelectMedia: (id) => _handleSelectMedia(context, id));
-      case TabType.LIBRARY:
-        return LibraryScreen(
-            onSelectMedia: (id) => _handleSelectMedia(context, id));
-    }
   }
 }
